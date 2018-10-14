009a6dcea7e72c0cf61631fd459e8bd0418c2a9f
Jason Rametta <rametta@outlook.com>
2018-10-14 09:30:33 -0400

    better error handling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const SVGO = require('svgo')
 const { trace } = require('potrace')
 const sizeOf = require('image-size')
@@ -9,14 +10,12 @@ const defaultOptions = {
 }
 
 // optimize :: String -> Promise String
-const optimize = svg => {
-  const svgo = new SVGO({ multipass: true, floatPrecision: 0 })
-  return svgo.optimize(svg)
-}
+const optimize = svg =>
+  new SVGO({ multipass: true, floatPrecision: 0 }).optimize(svg)
 
 // encodeOptimizedSVGDataUri :: String -> String
-function encodeOptimizedSVGDataUri(svgString) {
-  const uriPayload = encodeURIComponent(svgString) // encode URL-unsafe characters
+const encodeOptimizedSVGDataUri = svg => {
+  const uriPayload = encodeURIComponent(svg) // encode URL-unsafe characters
     .replace(/%0A/g, ``) // remove newlines
     .replace(/%20/g, ` `) // put spaces back in
     .replace(/%3D/g, `=`) // ditto equals signs
@@ -33,18 +32,40 @@ const format = data => size =>
     aspectRatio: (size.width / size.height).toFixed(3)
   })
 
+// exists :: String -> Promise String
+const exists = file =>
+  new Promise((res, rej) =>
+    fs.access(file, err => (err ? rej('ERROR: File not found') : res(file)))
+  )
+
+// size :: String -> Promise Size
+const size = file =>
+  new Promise((res, rej) =>
+    sizeOf(
+      file,
+      (err, sizes) =>
+        err ? rej('ERROR: Could not get size of image') : res(sizes)
+    )
+  )
+
+// outline :: Boolean -> Options -> String -> Size -> Promise String
+const outline = encode => options => path => sizes =>
+  new Promise((res, rej) =>
+    trace(path, options, (err, svg) => {
+      err
+        ? rej('ERROR: Could not create SVG trace')
+        : optimize(svg)
+            .then(({ data }) => (encode ? res(format(data)(sizes)) : res(data)))
+            .catch(() => rej('ERROR: Could not optimize SVG'))
+    })
+  )
+
+// createSvg :: Boolean -> Options -> String -> Promise String
+const createSvg = encode => options => path =>
+  size(path).then(outline(encode)(options)(path))
+
 // cute :: Config -> Promise String
 const cute = ({ filePath, encode = true, options = defaultOptions }) =>
-  new Promise((res, rej) => {
-    trace(filePath, options, (err, svg) => {
-      if (err) rej(err)
-      optimize(svg)
-        .then(
-          ({ data }) =>
-            encode ? res(format(data)(sizeOf(filePath))) : res(data)
-        )
-        .catch(e => rej(e))
-    })
-  })
+  exists(filePath).then(createSvg(encode)(options))
 
 module.exports = cute
